313d88254f251c195a60748de6e3e48be21e09b6
seo1016 <seoseunghun3@gmail.com>
2024-08-09 15:54:42 +0900

    feat: Add HackButton(1)

---
+++ hackdesignsystem/src/main/java/com/seo1016/hackdesignsystem/component/button/HackButton.kt
@@ -0,0 +1,17 @@
+package com.seo1016.hackdesignsystem.component.button
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun HackButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    text: String,
+    tint: Color = Color.Unspecified,
+    elevation: Float = 0.0f,
+    enabled: Boolean = true,
+) {
+
+}
