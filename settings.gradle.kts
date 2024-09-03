59e3ff0e9539af38479a75385367e268bb636b10
seo1016 <seoseunghun3@gmail.com>
2024-09-03 15:30:20 +0900

    fix: Release gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,10 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "8.5.2"
+        id("org.jetbrains.kotlin.android") version "1.9.22"
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
